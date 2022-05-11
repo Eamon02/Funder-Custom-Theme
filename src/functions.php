2c6b1c464188cf0dfcca2e5c05712451caa66b97
Eamon <eamonmchugh2@gmail.com>
2022-05-11 11:01:42 -0400

    reworked file structure and functions.php to upload to wordpress

--- src/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function divi__child_theme_enqueue_styles() {
-wp_enqueue_style( ‘parent-style’, get_template_directory_uri() . ‘/public/css/style.css’ );
-}add_action('wp_enque_scripts', 'divi__child_theme_enqueue_styles');
-function add_scripts(){
-    wp_enqueue_script( 'js', get_stylesheet_directory_uri() . '/public/js/switchPrice.js', array('jquery'), "1.0.0", false);
-} add_action('wp_enque_scripts', 'add_scripts');
-?>
