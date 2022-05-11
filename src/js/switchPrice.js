b6ba8cdd5de9a18aa4ea529d6ad2711ceca94e8c
Eamon <eamonmchugh2@gmail.com>
2022-05-11 12:29:57 -0400

    fixed switchPrice function. Caching issues with WP

--- src/js/switchPrice.js
+++ src/js/switchPrice.js
@@ -1,12 +1,12 @@
 function switchPrice(x) {
     if (x == 0) {
         console.log("show Quarterly")
-        $(".quartely--prices").css("display", "block");
-        $(".monthly--prices").css("display", "none");
+        $(".quartely--prices").css("display", "block !important");
+        $(".monthly--prices").css("display", "none !important");
     } else {
         console.log("show Monthly")
-        $(".quarterly--prices").css("display", "none");
-        $(".monthly--prices").css("display", "block");
+        $(".quarterly--prices").css("display", "none !important");
+        $(".monthly--prices").css("display", "block !important");
         return
     }
 }
